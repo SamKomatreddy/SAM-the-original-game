17ba4bfaf733636d468303ca1808982e04dc939f
SamKomatreddy <57407711+SamKomatreddy@users.noreply.github.com>
2019-11-05 10:17:55 -0600

    Create Sam-The-Original.py

---
+++ Sam-The-Original.py
@@ -0,0 +1,48 @@
+#Sam Komatreddy
+
+
+print("Welcome to the !!SAM game!!")
+print("Welcome to the !!SAM game!!")
+print("Welcome to the !!SAM game!!")
+print("Welcome to the !!SAM game!!")
+characterName = input("What name would you like ? ")                                                                                                                                                                                                                
+nameFront = "WELCOME TO THE GAME......." + characterName
+nameFront = nameFront.center(50)
+theOriginal = "The original Sam game developed by a short indian kid purely out fun of it,\
+this game just makes you laugh with some fun questions for my friends including but not limited to Korman, Arnav, Samarth, Erik, Harish, Shreya, Shivani\
+Dad, Mom, Mikki, Rushil, Arayak "
+theOriginal = theOriginal.center(50)
+#there are 50 spaces here
+print("+--------------------------------------------------+")
+print("|                                                  |")
+print("|                                                  |")
+print("|                                                  |")
+print("|        |------|       /\       |\            /|  |")
+print("|        |             /  \      | \          / |  |")
+print("|        |            /    \     |  \        /  |  |")
+print("|        |----|      /------\    |   \      /   |  |")
+print("|             |     /        \   |    \    /    |  |")
+print("|             |    /          \  |     \  /     |  |")
+print("|      |------|   /            \ |      \/      |  |")
+print("|                                                  |")
+print("|                                                  |")
+print("|                                                  |")
+print("|                                                  |")
+print("|                                                  |")
+print("|                                                  |")
+print("|                                                  |")
+print("|                                                  |")
+print("|"+nameFront+"|")#right here I used a center statement up top so I wouldn't have to actually find the exact number of something
+print("|                                                  |")
+print("|                                                  |")
+print("|                                                  |")
+print("|                                                  |")
+print("|                                                  |")
+print("|                                                  |")
+print("|                                                  |")
+print("|                                                  |")
+print("|                                                  |")
+print("|                                                  |")
+print("+--------------------------------------------------+")
+
+print("To start off the game "+characterName+", I am going to ask you a couple of questions about yourself?")
